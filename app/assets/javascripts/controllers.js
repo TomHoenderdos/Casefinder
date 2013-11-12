c28bfc1313fc95ecf6425b27c0b1fb1e45a54b55
Tom <info@tompc.nl>
2013-11-12 13:33:10 +0100

    decode the material string and replace it

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -2,6 +2,10 @@ var findMyCase = angular.module('findMyCase', []);
  
 findMyCase.controller('findMyCaseList', ['$scope', '$http', function($scope, $http){
 		$http.get('/cases.json').success(function(response) {
-			  $scope.allCases = response;
+			for (var key in response){
+			  	var res = response[key];
+	  			res.material = atob(res.material);
+			}
+			$scope.allCases = response;
 		});
 }]);
