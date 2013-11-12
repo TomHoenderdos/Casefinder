2aa02d05de303f583aaf1db2d7f6de600b687a77
Tom <info@tompc.nl>
2013-11-12 13:36:22 +0100

    revert from earlier commit, dont rewrite material just add new key material_decoded with the decoded value

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -4,7 +4,7 @@ findMyCase.controller('findMyCaseList', ['$scope', '$http', function($scope, $ht
 		$http.get('/cases.json').success(function(response) {
 			for (var key in response){
 			  	var res = response[key];
-	  			res.material = atob(res.material);
+	  			res.material_decoded = atob(res.material);
 			}
 			$scope.allCases = response;
 		});
