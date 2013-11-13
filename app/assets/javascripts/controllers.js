488f4ae5bb6253285c308768a4d0e8851e3a0992
Tom <info@tompc.nl>
2013-11-13 13:59:58 +0100

    new update

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -8,4 +8,43 @@ findMyCase.controller('findMyCaseList', ['$scope', '$http', function($scope, $ht
 			}
 			$scope.allCases = response;
 		});
-}]);
+		$scope.clearMaterial = function() {
+			$scope.materialFilter = {};
+		}
+
+		$scope.clearFinishing = function() {
+			$scope.finishing.Croco = "";
+			$scope.finishing.Print = "";
+			$scope.finishing.Snake = "";
+			$scope.finishing.Other = "";
+			$scope.finishing.Gloss = "";
+		}
+}])
+.filter('finishingFilter', function(){
+	return function(case_, finishing_) {
+		
+		if (finishing_ == undefined){
+			return case_;
+		}
+
+		var items = {
+			finishings: finishing_,
+			out: []
+		};
+		var allFalse = true;
+		angular.forEach(case_, function(value, key){	
+			console.log(this.finishings[value.finishing])
+			if (this.finishings[value.finishing] === true){
+				allFalse = false;
+				this.out.push(value);
+			}
+		}, items);
+
+		if (allFalse){
+			return case_;
+		} else {
+			return items.out;	
+		}
+		
+	};
+});
