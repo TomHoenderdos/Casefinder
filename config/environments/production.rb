9d302420ff963fd5fba29fdb1cd28eb8307f391d
Tom <info@tompc.nl>
2013-11-27 18:20:51 +0100

    Enabled caching of assets Diabled the Rails debug error messages when visiting a non existing url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ App::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # TODO: Change this while in production
   # Ensure you have defined default url options in your environments files. Here 
@@ -12,16 +12,19 @@ App::Application.configure do
   # in config/environments/development.rb:
   # In production, :host should be set to the actual host of your application.
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  # Set to :debug to see everything in the log.
+  config.log_level = :info
+  config.log_formatter = ::Logger::Formatter.new
 
-
+  
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -35,5 +38,5 @@ App::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 end
