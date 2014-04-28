da5cf897bddbff1c590208338928d195448fa901
David Anema <danema@mac.com>
2014-04-28 18:21:33 +0200

    Added best practice for SELECT filter

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -128,22 +128,16 @@ findMyCase.controller('findMyCaseList', ['$scope', '$http', function($scope, $ht
 	return function(allCases, sizeFilterInput){
 		if (!angular.isUndefined(allCases) && !angular.isUndefined(sizeFilterInput) && sizeFilterInput.length > 0) {
 			var tempCases = [];
-			angular.forEach(allCases, function(allCases_val, allCases_key){
-		
-		// Wanneer je sizes hebt toegevoegd zul je een functie kunnen gebruiken zoals:
-			
-			// angular.forEach(allCases, function(cases){	
-				// if (cases.sizes.indexOf(sizeFilterInput) != -1){
-					// tempCases.push(cases);
-				// }
-			// });
-		// Zie colorFilter als voorbeeld hiervan!
-
-				angular.forEach(allCases_val.devices, function(allCases_val, allCases_key){
-					if (allCases_key == sizeFilterInput){
-						tempCases.push(allCases_val);
+			angular.forEach(allCases, function(value, key){
+			var gotIt = false;	
+				angular.forEach(value.sizes, function(value, key){
+					if (value.size == sizeFilterInput){
+						gotIt=true;
 					}
 				});
+				if(gotIt){
+					tempCases.push(value);
+				}
 			});
 			return tempCases;
 		} else {
